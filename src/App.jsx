6ae12ec54f343ceced916c5b5ab45948f42e800c
Dollardudez <robclancy.98@gmail.com>
2021-08-24 09:20:42 -0500

    added more styling, completely installed TailwindCSS, and added a picture for my world cup project!

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Home from './components/Home';
 import About from './components/About';
 import Navbar from './components/About';
 import Contact from './components/Contact';
---
+++ src/data.js
@@ -0,0 +1,36 @@
+// src/data.js
+
+export const projects = [
+    {
+      title: "Restaurant Review",
+      subtitle: "PERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-1.gif",
+      link: "https://www.robclancyprojects.com/",
+    },
+    {
+      title: "Rocket Man",
+      subtitle: "Python and Pygame",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "",
+    },
+    {
+      title: "Manhattan Chests",
+      subtitle: "Server-side rendering using EJS, Node, Sqlite3, and Express",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://peaceful-sierra-47377.herokuapp.com/",
+    },
+    {
+      title: "World Cup Database",
+      subtitle: "Microsoft SQL Server, C#, WPF, and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./media/world-cup-pic.png",
+      link: "",
+    },
+  ];
