9401e52965890f9f7cd522c4994803bd819d23ed
Dollardudez <robclancy.98@gmail.com>
2021-08-29 11:42:33 -0500

    fixed pdf loading issue in production environment... hopefully ;)

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
-import samplePDF from '../robert_clancy_2021.pdf';
+import { Document, Page, pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 
 
 const Resume = () => {
@@ -8,7 +9,7 @@ const Resume = () => {
     return (
         <section id="resume">
           <div className="flex flex-col w-full items-center">
-              <Document file={samplePDF}>
+              <Document file={"./media/robert_clancy_2021.pdf"}>
                 <Page pageNumber={1} scale={1.5}/>
               </Document>
           </div>
